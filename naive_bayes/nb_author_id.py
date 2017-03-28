bf273793749b3c1c75ac3b1bea260ddad8906099
jihao <jacky.jihao@gmail.com>
2017-03-28 21:10:04 +0800

    NB

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -19,14 +19,19 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(words_file = "../tools/word_data_unix.pkl", authors_file="../tools/email_authors_unix.pkl")
 
 
 
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+print(clf.score(features_test,labels_test))
 
 #########################################################
 
