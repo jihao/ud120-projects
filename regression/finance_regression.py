231f7e613e059e81d0ab62632a19c460860fced9
jihao <jacky.jihao@gmail.com>
2017-06-02 21:57:37 +0800

    7. regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,19 +17,23 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb"), fix_imports=True )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified_unix.pkl", "rb"), fix_imports=True )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+
+# 7-45 根据 long_term_incentive LTI 回归奖金
+#features_list = ["bonus", "long_term_incentive" ]
+
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys_unix.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,9 +41,27 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+
+### get the slope
+### again, you'll get a 2-D array, so stick the [0][0] at the end
+slope = reg.coef_ ### fill in the line of code to get the right value
+
+### get the intercept
+### here you get a 1-D array, so stick [0] on the end to access
+### the info we want
+intercept = reg.intercept_ ### fill in the line of code to get the right value
+
+                          
+### get the score on test data
+test_score = reg.score(feature_test, target_test) ### fill in the line of code to get the right value
 
 
-
+### get the score on the training data
+training_score = reg.score(feature_train, target_train)### fill in the line of code to get the right value
 
 
 
@@ -64,6 +86,17 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+# 7-47 异常值破坏回归
+"""
+现在，我们将绘制两条回归线，一条在测试数据上拟合（有异常值），一条在训练数据上拟合（无异常值）。来看看现在的图形，有很大差别，对吧？单一的异常值会引起很大的差异。
+
+新的回归线斜率是多少？
+"""
+reg.fit(feature_test, target_test)
+slope2 = reg.coef_
+
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
