47b2bdff9f0ec734d1412d84f4cb9c14ceac816b
jihao <jacky.jihao@gmail.com>
2017-06-04 08:54:18 +0800

    8. outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -8,12 +8,24 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") fix_imports = True )
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb"), fix_imports = True )
+data_dict.pop( "TOTAL", 0 )
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+# 去除 TOTAL 之后的数据点，是poi数据，不是要去除的异常值
 
+max_salary = 0
+for point in data:
+    salary = point[0]
+    if salary > max_salary:
+        max_salary = salary
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -9,8 +9,8 @@ from outlier_cleaner import outlierCleaner
 
 
 ### load up some practice data with outliers in it
-ages = pickle.load(open("practice_outliers_ages.pkl", "rb"), fix_imports=True)
-net_worths = pickle.load(open("practice_outliers_net_worths.pkl", "rb"), fix_imports=True)
+ages = pickle.load(open("practice_outliers_ages_unix.pkl", "rb"), fix_imports=True)
+net_worths = pickle.load(open("practice_outliers_net_worths_unix.pkl", "rb"), fix_imports=True)
 
 
 
@@ -26,11 +26,15 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+### get the slope
+slope = reg.coef_ ### fill in the line of code to get the right value
 
-
-
+test_score = reg.score(ages_test, net_worths_test)
 
 
 
@@ -68,6 +72,10 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        
+        slope2 = reg.coef_ 
+        test_score2 = reg.score(ages_test, net_worths_test)
+        
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print ("you don't seem to have regression imported/created,")
