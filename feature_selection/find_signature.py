8e24351233345c63fc3e7463ba6efb2c47e7ff03
jihao <jacky.jihao@gmail.com>
2017-06-22 20:46:31 +0800

    12. feature seletcion

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -10,8 +10,8 @@ numpy.random.seed(42)
 ### mini-project.
 words_file = "../text_learning/your_word_data.pkl" 
 authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+word_data = pickle.load( open(words_file, "rb"),fix_imports=True)
+authors = pickle.load( open(authors_file, "rb"),fix_imports=True )
 
 
 
@@ -19,25 +19,71 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+# from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
-features_train = vectorizer.fit_transform(features_train)
+features_train = vectorizer.fit_transform(features_train).toarray()
 features_test  = vectorizer.transform(features_test).toarray()
 
+feature_names= vectorizer.get_feature_names()  
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
-features_train = features_train[:150].toarray()
+features_train = features_train[:150]
 labels_train   = labels_train[:150]
 
 
 
 ### your code goes here
+from time import time
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier()
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time()-t0, 3), "s")
+acc = accuracy_score(pred, labels_test)
+print (acc) 
 
+# How many training points are there, according to the starter code?
+print(len(features_train))
+
+# What’s the importance of the most important feature? What is the number of this feature?
+importances = clf.feature_importances_
+for index, item in enumerate(importances):
+    if item > 0.2:        
+        print (index, item)       
+       
+import numpy as np
+indices = np.argsort(importances)[::-1]
+print ('Feature Ranking: ')
+for i in range(10):
+    print ("{} feature no.{} ({})".format(i+1,indices[i],importances[indices[i]]))
+
+# What’s the most powerful word when your decision tree is makeing its classification decisions?
+feature_name = vectorizer.get_feature_names()
+for index, item in enumerate(feature_name):
+    if index == 33614:        
+        print (item)
+
+print(vectorizer.get_feature_names()[33614])
+# Result: sshacklensf      
+        
+feature_name = vectorizer.get_feature_names()
+for index, item in enumerate(feature_name):
+    if index == 14343:        
+        print (item) 
+        
+print(vectorizer.get_feature_names()[14343])
+# Result: cgermannsf
 
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,17 +44,20 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         #temp_counter += 1
         #if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print (path)
+            #print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
             text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-            for word in ["sara", "shackleton", "chris", "germani"]:
+            ### for word in ["sara", "shackleton", "chris", "germani"]:
+            
+            ### lesson 12 - feature selection 
+            for word in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
                 text = text.replace(word, '')
             ### append the text to word_data
- 
+            
             word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
             if name=="sara":
@@ -68,14 +71,15 @@ print ("emails processed")
 from_sara.close()
 from_chris.close()
 
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
             
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer  = TfidfVectorizer(stop_words="english"  )
-word_data = vectorizer.fit_transform(word_data) 
+X = vectorizer.fit_transform(word_data) 
 feature_names= vectorizer.get_feature_names()          
             
-pickle.dump( word_data, open("your_word_data.pkl", "wb") )
-pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
+
 
 
 
